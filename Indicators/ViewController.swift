86bc6949cc38bcb7bed6f50c06c8c53375c899fb
Igor Nabokov <inabokov@65apps.com>
2017-12-17 18:34:08 +0400

    Initial Commit

---
+++ Indicators/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Indicators
+//
+//  Created by Igor Nabokov on 17/12/2017.
+//  Copyright © 2017 Igor Nabokov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
